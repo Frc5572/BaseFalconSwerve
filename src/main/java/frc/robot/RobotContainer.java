b391157adb9c9c4c6e1001aea0951595abc57ab9
Owen <owenjflatman@gmail.com>
2023-01-26 18:51:40 -0600

    April tag testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -78,7 +78,7 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
 
         return new TestTransform(s_Swerve,
-            new Transform2d(new Translation2d(0, 0), Rotation2d.fromDegrees(180)), 3);
+            new Transform2d(new Translation2d(0, 0), Rotation2d.fromDegrees(180)), 2);
 
         // return autoChooser.getSelected();
 
